3a493166e9835a1975320b3d0579fad5cf4f5d63
nell <devstory.co.kr@gmail.com>
2025-05-28 12:57:16 +0900

    Initial Commit

---
+++ guita/guitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  guitaApp.swift
+//  guita
+//
+//  Created by Nell on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct guitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
