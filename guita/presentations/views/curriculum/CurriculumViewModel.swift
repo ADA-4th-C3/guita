3e4bb038fe8ca9a3681b62ca48c575ff11dcc72c
00jup <jupark0369@gmail.com>
2025-05-30 11:07:11 +0900

    Refact: Folder File Structure Refact

--- guita/presentations/views/curriculum/CurriculumViewModel.swift
+++
@@ -1,7 +0,0 @@
-//  Copyright © 2025 ADA 4th Challenge3 Team1. All rights reserved.
-
-final class CurriculumViewModel: BaseViewModel<CurriculumViewState> {
-  init() {
-    super.init(state: .init())
-  }
-}
