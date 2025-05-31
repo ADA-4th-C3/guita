3b8df13909d7491188502182ea27aa280592d012
Isla <Islajiin@gmail.com>
2025-05-31 14:00:44 +0900

    Feat: implement initial layout for CurriculumView

--- gwita/presentations/views/curriculum/CurriculumView.swift
+++ gwita/presentations/views/curriculum/CurriculumView.swift
@@ -9,12 +9,98 @@ struct CurriculumView: View {
     ) { _, _ in
       VStack {
         // MARK: Toolbar
-        Toolbar(title: "Curriculum")
+        Toolbar(title: "학습 목록")
         Spacer()
+        
+        ScrollView {
+          LazyVStack(alignment:.leading, spacing:8)
+          {
+            HStack {
+              HStack {
+                Text("[초급 1]")
+                  .font(.system(size:18))
+                  .fontWeight(.bold)
+                Text("여행을 떠나요")
+                  .font(.system(size:18))
+                  .fontWeight(.bold)
+              }.padding(.horizontal,30)
+              Spacer()
+              
+              HStack {
+                Text ("A")
+                  .font(.system(size:17))
+                  .foregroundColor(.black)
+                  .padding(.horizontal,6)
+                  .background(Color.white)
+                  .cornerRadius(5)
+                
+                
+                Text ("A")
+                  .font(.system(size:17))
+                  .padding(.horizontal,6)
+                  .foregroundColor(.black)
+                  .background(Color.white)
+                  .cornerRadius(5)
+                
+                
+                Text ("A")
+                  .font(.system(size:17))
+                  .foregroundColor(.black)
+                  .padding(.horizontal,6)
+                  .background(Color.white)
+                  .cornerRadius(5)
+              }
+              .padding(.horizontal)
+            }
+            .frame(width:393, height:110)
+            
+            HStack {
+              HStack {
+                Text("[초급 1]")
+                  .font(.system(size:18))
+                  .fontWeight(.bold)
+                Text("여행을 떠나요")
+                  .font(.system(size:18))
+                  .fontWeight(.bold)
+              }.padding(.horizontal,30)
+              Spacer()
+              
+              HStack {
+                Text ("A")
+                  .font(.system(size:17))
+                  .foregroundColor(.black)
+                  .padding(.horizontal,6)
+                  .background(Color.white)
+                  .cornerRadius(5)
+                
+                
+                Text ("A")
+                  .font(.system(size:17))
+                  .padding(.horizontal,6)
+                  .foregroundColor(.black)
+                  .background(Color.white)
+                  .cornerRadius(5)
+                
+                
+                Text ("A")
+                  .font(.system(size:17))
+                  .foregroundColor(.black)
+                  .padding(.horizontal,6)
+                  .background(Color.white)
+                  .cornerRadius(5)
+              }
+              .padding(.horizontal)
+            }
+            .frame(width:393, height:110)
+            
+            
+          }
+        }
       }
-    }
+    } 
   }
 }
+
 
 #Preview {
   BasePreview {
