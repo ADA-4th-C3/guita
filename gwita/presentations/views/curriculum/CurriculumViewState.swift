2d2bcdb2ae51f9c8e0fda0c3ee0e4d90fdb4209f
Isla <Islajiin@gmail.com>
2025-05-31 11:56:12 +0900

    Feat: add CurriculumViewState

--- gwita/presentations/views/curriculum/CurriculumViewState.swift
+++ gwita/presentations/views/curriculum/CurriculumViewState.swift
@@ -1,3 +1,20 @@
 //  Copyright © 2025 ADA 4th Challenge3 Team1. All rights reserved.
 
-struct CurriculumViewState {}
+import Foundation
+
+struct CurriculumViewState {
+  let id = UUID()
+  let level: String
+  let title: String
+  let chords: [String]
+  let isSelected :Bool
+  
+  func copy(level: String? = nil,title: String? = nil,chords: [String]? = nil,isSelected: Bool? = nil) -> CurriculumViewState {
+    return CurriculumViewState(
+      level: level ?? self.level,
+      title: title ?? self.title,
+      chords: chords ?? self.chords,
+      isSelected: isSelected ?? self.isSelected
+    )
+  }
+}
