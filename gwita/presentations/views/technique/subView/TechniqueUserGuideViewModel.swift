6ed274b26a6709d3f59ac65e9cbeced0efba6bee
Isla <Islajiin@gmail.com>
2025-06-01 16:40:56 +0900

    Feat: implement TechniqueUserGuideView, ViewModel, ViewState

---
+++ gwita/presentations/views/technique/subView/TechniqueUserGuideViewModel.swift
@@ -0,0 +1,8 @@
+//  Copyright © 2025 ADA 4th Challenge3 Team1. All rights reserved.
+
+
+final class TechniqueUserGuideViewModel: BaseViewModel<TechniqueUserGuideViewState> {
+  init() {
+    super.init(state: .init())
+  }
+}
