591f176ae2e2b9f78a12fafe222c859cd6ca36f8
Isla <Islajiin@gmail.com>
2025-06-01 16:42:20 +0900

    Refact: add showInfo for UserGuide

--- gwita/features/toolbar/CustomToolbar.swift
+++ gwita/features/toolbar/CustomToolbar.swift
@@ -2,34 +2,45 @@
 import SwiftUI
 
 struct CustomToolbar: View {
-    var title: String
-    var onBack: () -> Void
-    var onInfo: () -> Void
-
-    var body: some View {
-        HStack {
-            Button(action: {onBack()}) {
-                Image("arrow-trailing")
-                .resizable()
-                .frame(width: 24, height:24)
-            }
-            .padding(.leading, 16)
-
-            Spacer()
-
-            Text(title)
-            .font(.custom("KoddiUDOnGothic-Bold.ttf",size: 24))
-
-            Spacer()
-
-            Button(action: {onInfo()}) {
-                Image("info")
-                .resizable()
-                .frame(width: 24, height:24)
-            }
-            .padding(.trailing, 16)
+  var title: String
+  var onBack: () -> Void
+  var onInfo: (() -> Void)? = nil
+  var showInfo: Bool = true
+  
+  var body: some View {
+    HStack {
+      Button(action: {onBack()}) {
+        Image("arrow-trailing")
+          .resizable()
+          .frame(width: 24, height:24)
+      }
+      .padding(.leading, 16)
+      
+      Spacer()
+      
+      Text(title)
+        .font(.custom("KoddiUDOnGothic-Bold",size: 24))
+      
+      Spacer()
+      
+      Group {
+        if showInfo {
+          Button(action: {
+            onInfo?()
+          }) {
+            Image("info")
+              .resizable()
+              .frame(width: 24, height:24)
+          }
+        } else {
+          Color.clear
+            .frame(width: 24, height:24)
+          
         }
-        .frame(height: 44)
-
+      }
+      .padding(.trailing, 16)
     }
+    .frame(height: 44)
+    
+  }
 }
